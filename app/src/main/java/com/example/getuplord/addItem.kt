4b7bf9e5eade608ed35d079f046cd7149ad5fb68
Fergus <fme36@uclive.ac.nz>
2020-05-24 18:09:52 +1200

    got photos stored, retreived and displayed!

--- app/src/main/java/com/example/getuplord/addItem.kt
+++ app/src/main/java/com/example/getuplord/addItem.kt
@@ -7,6 +7,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Size
 import android.graphics.Matrix
 import android.net.Uri
@@ -43,6 +44,7 @@ class addItem : AppCompatActivity() , LifecycleOwner {
 
     lateinit var imagePreview: ImageView
     lateinit var takePhoto: Button
+    lateinit var imageBitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +57,20 @@ class addItem : AppCompatActivity() , LifecycleOwner {
         takePhoto.setOnClickListener {
             dispatchTakePictureIntent()
         }
+
+
+
+        BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.getuplord/files/Pictures/JPEG_testPic1_7510796440909568314.jpg")?.also { bitmap ->
+            imageBitmap = bitmap
+        }
+
+        val matrix = Matrix()
+        matrix.postRotate(90F)
+
+        val scaledBitmap : Bitmap = Bitmap.createScaledBitmap(imageBitmap, 640, 640, true)
+        val rotatedImage: Bitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, 640, 640, matrix, true)
+
+        imagePreview.setImageBitmap(rotatedImage)
 
         // Add this at the end of onCreate function
 
@@ -98,7 +114,8 @@ class addItem : AppCompatActivity() , LifecycleOwner {
                         "com.example.android.fileprovider",
                         it
                     )
-                    Log.d("check1", "photoURI created successfully")
+                    val photoPath: String = photoFile.invariantSeparatorsPath
+                    Log.d("check1", photoPath)
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
                 }
@@ -114,13 +131,16 @@ class addItem : AppCompatActivity() , LifecycleOwner {
 //        }
 //    }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Log.d("check2", "getting into if statement")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Log.d("check2", "getting into if statement")
 //            val imageBitmap = data?.extras?.get("data") as Bitmap
 //            imagePreview.setImageBitmap(imageBitmap)
-//        }
-//    }
+            BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.getuplord/files/Pictures/JPEG_testPic1_3304633960654210402.jpg")?.also { bitmap ->
+                imagePreview.setImageBitmap(bitmap)
+            }
+        }
+    }
 
     lateinit var currentPhotoPath: String
 
@@ -138,132 +158,6 @@ class addItem : AppCompatActivity() , LifecycleOwner {
             currentPhotoPath = absolutePath
         }
     }
-
-
-//    private val executor = Executors.newSingleThreadExecutor()
-//    private lateinit var viewFinder: TextureView
-//
-//    private fun startCamera() {
-//        // Create configuration object for the viewfinder use case
-//        val previewConfig = PreviewConfig.Builder().apply {
-//            setTargetResolution(Size(640, 480))
-//        }.build()
-//
-//
-//        // Build the viewfinder use case
-//        val preview = Preview(previewConfig)
-//
-//        // Every time the viewfinder is updated, recompute layout
-//        preview.setOnPreviewOutputUpdateListener {
-//
-//            // To update the SurfaceTexture, we have to remove it and re-add it
-//            val parent = viewFinder.parent as ViewGroup
-//            parent.removeView(viewFinder)
-//            parent.addView(viewFinder, 0)
-//
-//            viewFinder.surfaceTexture = it.surfaceTexture
-//            updateTransform()
-//        }
-//
-//        // Add this before CameraX.bindToLifecycle
-//
-//        // Create configuration object for the image capture use case
-//        val imageCaptureConfig = ImageCaptureConfig.Builder()
-//            .apply {
-//                // We don't set a resolution for image capture; instead, we
-//                // select a capture mode which will infer the appropriate
-//                // resolution based on aspect ration and requested mode
-//                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
-//            }.build()
-//
-//        // Build the image capture use case and attach button click listener
-//        val imageCapture = ImageCapture(imageCaptureConfig)
-//        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
-//            val file = File(externalMediaDirs.first(),
-//                "${System.currentTimeMillis()}.jpg")
-//
-//            imageCapture.takePicture(file, executor,
-//                object : ImageCapture.OnImageSavedListener {
-//                    override fun onError(
-//                        imageCaptureError: ImageCapture.ImageCaptureError,
-//                        message: String,
-//                        exc: Throwable?
-//                    ) {
-//                        val msg = "Photo capture failed: $message"
-//                        Log.e("CameraXApp", msg, exc)
-//                        viewFinder.post {
-//                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
-//
-//                    override fun onImageSaved(file: File) {
-//                        val msg = "Photo capture succeeded: ${file.absolutePath}"
-//                        Log.d("CameraXApp", msg)
-//                        viewFinder.post {
-//                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
-//                })
-//        }
-//
-//        CameraX.bindToLifecycle(this, preview, imageCapture)
-//    }
-//
-//    private fun updateTransform() {
-//        val matrix = Matrix()
-//
-//        val centerX = viewFinder.width / 2f
-//        val centerY = viewFinder.height / 2f
-//
-//        val rotationDegrees = when(viewFinder.display.rotation) {
-//            Surface.ROTATION_0 -> 0
-//            Surface.ROTATION_90 -> 90
-//            Surface.ROTATION_180 -> 180
-//            Surface.ROTATION_270 -> 270
-//            else -> return
-//        }
-//        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
-//
-//        viewFinder.setTransform(matrix)
-//    }
-//
-//    /**
-//     * Process result from permission request dialog box, has the request
-//     * been granted? If yes, start Camera. Otherwise display a toast
-//     */
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-//        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-//            if (allPermissionsGranted()) {
-//                viewFinder.post { startCamera() }
-//            } else {
-//                Toast.makeText(this,
-//                    "Permissions not granted by the user.",
-//                    Toast.LENGTH_SHORT).show()
-//                finish()
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Check if all permission specified in the manifest have been granted
-//     */
-//    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-//        ContextCompat.checkSelfPermission(
-//            baseContext, it) == PackageManager.PERMISSION_GRANTED
-//    }
-//
-//    //code from alternate method
-//    // Exercise 5
-//    private fun takePictureFromCamera() {
-//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//        intent.resolveActivity(packageManager)?.let {
-//            //code for
-////            val uri = dayUri(year, month, day)
-//            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-//            startActivityForResult(intent, REQUEST_CAMERA)
-//        }
-//    }
 
 
 
